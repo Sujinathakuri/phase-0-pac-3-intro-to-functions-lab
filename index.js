e440c21923f040ff5831015139acf843741d9d17
Sujinathakuri <sujchand2@gmail.com>
2021-04-15 09:13:32 +0545

    Completed assignment

---
+++ index.js
@@ -0,0 +1,7 @@
+shout(string)
+receives one argument and returns it in all caps:
+    Error: Expected undefined to equal 'HELLO'
+at assert(node_modules / expect / lib / assert.js: 29: 9)
+at Expectation.toEqual(node_modules / expect / lib / Expectation.js: 81: 30)
+at Context.(test / index - test.js: 4: 28)
+at processImmediate(internal / timers.js: 461: 21)
